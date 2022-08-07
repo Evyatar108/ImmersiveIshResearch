695786bec7ea1d52788adec80f10daf0630f0c5d
MIDDLEEAST\evmitran <evmitran@microsoft.com>
2022-08-07 02:50:17 -0700

    new textures

--- Source/ImmersiveIshResearch/Utils/ImmersiveResearchTextures.cs
+++ Source/ImmersiveIshResearch/Utils/ImmersiveResearchTextures.cs
@@ -12,19 +12,20 @@ namespace ImmersiveResearch
         public static readonly Texture2D ResearchSizeEssentialIcon = ContentFinder<Texture2D>.Get("UI/Icons/essential");
         public static readonly Texture2D ResearchSizeModIcon = ContentFinder<Texture2D>.Get("UI/Icons/mod");
 
-        public static readonly Texture2D MechanicalIcon = ContentFinder<Texture2D>.Get("UI/Icons/mechanical");
-        public static readonly Texture2D BiologicalIcon = ContentFinder<Texture2D>.Get("UI/Icons/biological");
-        public static readonly Texture2D TextilesIcon = ContentFinder<Texture2D>.Get("UI/Icons/textiles");
-        public static readonly Texture2D CulturalIcon = ContentFinder<Texture2D>.Get("UI/Icons/Cultural");
-        public static readonly Texture2D ConstructionIcon = ContentFinder<Texture2D>.Get("UI/Icons/Construction");
-        public static readonly Texture2D MetallurgyIcon = ContentFinder<Texture2D>.Get("UI/Icons/metallurgy");
-        public static readonly Texture2D WeaponryIcon = ContentFinder<Texture2D>.Get("UI/Icons/weaponry");
-        public static readonly Texture2D ElectricalIcon = ContentFinder<Texture2D>.Get("UI/Icons/electrical");
-        public static readonly Texture2D MedicalIcon = ContentFinder<Texture2D>.Get("UI/Icons/medical");
-        public static readonly Texture2D AdvancedIcon = ContentFinder<Texture2D>.Get("UI/Icons/advanced");
-        public static readonly Texture2D SpacerIcon = ContentFinder<Texture2D>.Get("UI/Icons/spacer");
-        public static readonly Texture2D UltratechIcon = ContentFinder<Texture2D>.Get("UI/Icons/ultratech");
-        public static readonly Texture2D SpacecraftIcon = ContentFinder<Texture2D>.Get("UI/Icons/spacecraft");
+        public static readonly Texture2D MechanicalIcon = ContentFinder<Texture2D>.Get("UI/Icons/mechanical_v2");
+        public static readonly Texture2D BiologicalIcon = ContentFinder<Texture2D>.Get("UI/Icons/biological_v2");
+        public static readonly Texture2D TextilesIcon = ContentFinder<Texture2D>.Get("UI/Icons/textiles_v2");
+        public static readonly Texture2D CulturalIcon = ContentFinder<Texture2D>.Get("UI/Icons/Cultural_v2");
+        public static readonly Texture2D ConstructionIcon = ContentFinder<Texture2D>.Get("UI/Icons/Construction_v2");
+        public static readonly Texture2D MetallurgyIcon = ContentFinder<Texture2D>.Get("UI/Icons/metallurgy_v2");
+        public static readonly Texture2D WeaponryIcon = ContentFinder<Texture2D>.Get("UI/Icons/weaponry_v2");
+        public static readonly Texture2D ElectricalIcon = ContentFinder<Texture2D>.Get("UI/Icons/electrical_v2");
+        public static readonly Texture2D MedicalIcon = ContentFinder<Texture2D>.Get("UI/Icons/medical_v2");
+        public static readonly Texture2D AdvancedIcon = ContentFinder<Texture2D>.Get("UI/Icons/advanced_v2");
+        public static readonly Texture2D SpacerIcon = ContentFinder<Texture2D>.Get("UI/Icons/spacer_v2");
+        public static readonly Texture2D UltratechIcon = ContentFinder<Texture2D>.Get("UI/Icons/ultratech_v2");
+        public static readonly Texture2D SpacecraftIcon = ContentFinder<Texture2D>.Get("UI/Icons/spacecraft_v2");
         public static readonly Texture2D ModIcon = ContentFinder<Texture2D>.Get("UI/Icons/mod");
+        public static readonly Texture2D UncategorizedIcon = ContentFinder<Texture2D>.Get("UI/Icons/Uncategorized");
     }
 }
